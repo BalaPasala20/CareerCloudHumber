aef2befba67595de50ffbf89522b383342157ab8
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-11-14 16:21:20 -0500

    Adding ApplicationEducationRepository with CRUD implementation

---
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -0,0 +1,153 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantEducationRepository : BaseAdo, IDataRepository<ApplicantEducationPoco>
+    {
+        public void Add(params ApplicantEducationPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            { 
+                SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+                foreach (ApplicantEducationPoco item in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[Applicant_Educations]
+                                   ([Id]
+                                   ,[Applicant]
+                                   ,[Major]
+                                   ,[Certificate_Diploma]
+                                   ,[Start_Date]
+                                   ,[Completion_Date]
+                                   ,[Completion_Percent])
+                             VALUES
+                                   (@Id, 
+                                   ,@Applicant
+                                   ,@Major
+                                   ,@CertificateDiploma
+                                   ,@StartDate
+                                   ,@CompletionDate
+                                   ,@CompletionPercent)";
+                    cmd.Parameters.AddWithValue("@Id", item.Id);
+                    cmd.Parameters.AddWithValue("@Applicant", item.Applicant);
+                    cmd.Parameters.AddWithValue("@Major", item.Major);
+                    cmd.Parameters.AddWithValue("@CertificateDiploma", item.CertificateDiploma);
+                    cmd.Parameters.AddWithValue("@StartDate", item.StartDate);
+                    cmd.Parameters.AddWithValue("@CompletionDate", item.CompletionDate);
+                    cmd.Parameters.AddWithValue("@CompletionPercent", item.CompletionPercent);
+
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT *
+                              FROM [dbo].[Applicant_Educations]";
+                conn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                int counter = 0;
+                ApplicantEducationPoco[] pocos = new ApplicantEducationPoco[1000]; //choosing a max number
+                //learning notes: connection has to be open to loop through the reader. Executereader returns the handle to the cursor pointing
+                //to the record in the database.
+                while(rdr.Read())
+                {
+                    ApplicantEducationPoco poco = new ApplicantEducationPoco();
+                    poco.Id = rdr.GetGuid(0);
+                    poco.Applicant = rdr.GetGuid(1);
+                    poco.Major = rdr.GetString(2);
+                    poco.CertificateDiploma = rdr.GetString(3);
+                    poco.StartDate = rdr.IsDBNull(4) ? (DateTime?) null : rdr.GetDateTime(4);
+                    poco.CompletionDate = (DateTime?)rdr.GetSqlValue(5);  // we can also use the syntax used for StartDate
+                    poco.CompletionPercent = (byte?)rdr[6]; //another way
+                    poco.TimeStamp = (byte[])rdr["TimeStamp"]; //you can also use the column name
+                    pocos[counter] = poco;
+                    counter++;
+                }
+                conn.Close();
+
+                return pocos.Where(p => p != null).ToList();
+            }
+        }
+
+        public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantEducationPoco GetSingle(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            IQueryable<ApplicantEducationPoco> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
+        }
+
+        public void Remove(params ApplicantEducationPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (ApplicantEducationPoco item in items)
+                {
+                    cmd.CommandText = @"DELETE FROM[dbo].[Applicant_Educations]
+                                        WHERE [Id] = @Id";
+                    cmd.Parameters.AddWithValue("@Id", item.Id);
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Update(params ApplicantEducationPoco[] items)
+        {
+            using(SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach(ApplicantEducationPoco item in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Applicant_Educations]
+                                       SET [Applicant] = @Applicant
+                                        ,[Major] = @Major
+                                        ,[Certificate_Diploma] = @Certificate_Diploma
+                                        ,[Start_Date] = @Start_Date
+                                        ,[Completion_Date] = @Completion_Date
+                                        ,[Completion_Percent] = @Completion_Percent
+                                       WHERE [Id] = @Id";
+                    cmd.Parameters.AddWithValue("@Id", item.Id);
+                    cmd.Parameters.AddWithValue("@Applicant", item.Applicant);
+                    cmd.Parameters.AddWithValue("@Major", item.Major);
+                    cmd.Parameters.AddWithValue("@CertificateDiploma", item.CertificateDiploma);
+                    cmd.Parameters.AddWithValue("@StartDate", item.StartDate);
+                    cmd.Parameters.AddWithValue("@CompletionDate", item.CompletionDate);
+                    cmd.Parameters.AddWithValue("@CompletionPercent", item.CompletionPercent);
+
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/BaseAdo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public abstract class BaseAdo
+    {
+        protected string _connectionString;
+        public BaseAdo()
+        {
+            ConfigurationBuilder config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connectionString = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+    }
+}
