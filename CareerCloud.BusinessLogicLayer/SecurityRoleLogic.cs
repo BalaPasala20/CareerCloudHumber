f7e2d79b6d18e80fc9d3a13a3fb37709eaf30de0
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 15:34:47 -0500

    adding business logic for SecurityRolePoco

---
+++ CareerCloud.BusinessLogicLayer/SecurityRoleLogic.cs
@@ -0,0 +1,41 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SecurityRoleLogic : BaseLogic<SecurityRolePoco>
+    {
+        public SecurityRoleLogic(IDataRepository<SecurityRolePoco> repo) : base(repo)
+        {
+
+        }
+        public override void Add(SecurityRolePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(SecurityRolePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(SecurityRolePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (SecurityRolePoco poco in pocos)
+            {
+                if(string.IsNullOrEmpty(poco.Role))
+                {
+                    exceptions.Add(new ValidationException(800, $"{poco.Id} - Role cannot be empty"));
+                }
+                if(exceptions.Count >0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
