98d29b315c15cb2908aa8676a6b8e3c338c3c0d6
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 15:53:39 -0500

    adding business logic for SystemLanguageCodePoco

---
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SystemLanguageCodeLogic
+    {
+        private IDataRepository<SystemLanguageCodePoco> _repository;
+        public SystemLanguageCodeLogic(IDataRepository<SystemLanguageCodePoco> repo)
+        {
+            _repository = repo;
+        }
+
+        public SystemLanguageCodePoco Get(String languageId)
+        {
+            return _repository.GetSingle(c => c.LanguageID == languageId);
+        }
+
+        public List<SystemLanguageCodePoco> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+        protected void Verify(SystemLanguageCodePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (SystemLanguageCodePoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.LanguageID))
+                {
+                    exceptions.Add(new ValidationException(1000, "LanguageID Cannot be empty"));
+                }
+                if (string.IsNullOrEmpty(poco.Name))
+                {
+                    exceptions.Add(new ValidationException(1001, "Name Cannot be empty"));
+                }
+                if (string.IsNullOrEmpty(poco.NativeName))
+                {
+                    exceptions.Add(new ValidationException(1002, "NativeName Cannot be empty"));
+                }
+                if (exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+
+        public void Add(SystemLanguageCodePoco[] pocos)
+        {
+            Verify(pocos);
+            _repository.Add(pocos);
+        }
+
+        public void Update(SystemLanguageCodePoco[] pocos)
+        {
+            Verify(pocos);
+            _repository.Update(pocos);
+        }
+
+        public void Delete(SystemLanguageCodePoco[] pocos)
+        {
+            _repository.Remove(pocos);
+        }
+    }
+}
