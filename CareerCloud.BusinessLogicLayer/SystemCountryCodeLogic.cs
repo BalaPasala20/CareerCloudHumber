07284dceeea1851017a4c8de44d6cd5d2aac99f2
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 15:33:17 -0500

    adding business logic for SystemCountryCodePoco

---
+++ CareerCloud.BusinessLogicLayer/SystemCountryCodeLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+using System.Linq;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SystemCountryCodeLogic 
+    {
+        private readonly IDataRepository<SystemCountryCodePoco> _repository;
+
+        public SystemCountryCodeLogic(IDataRepository<SystemCountryCodePoco> repo)
+        {
+			_repository = repo;
+        }		
+        public SystemCountryCodePoco Get(String code)
+        {
+            return _repository.GetSingle(c => c.Code == code);
+        }
+
+        public List<SystemCountryCodePoco> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public void Add(SystemCountryCodePoco[] pocos)
+        {
+            Verify(pocos);
+            _repository.Add(pocos);
+        }
+
+        public void Update(SystemCountryCodePoco[] pocos)
+        {
+            Verify(pocos);
+            _repository.Update(pocos);
+        }
+        protected void Verify(SystemCountryCodePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (SystemCountryCodePoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Code))
+                {
+                    exceptions.Add(new ValidationException(900, $"Code Cannot be empty"));
+                }
+                if (string.IsNullOrEmpty(poco.Name))
+                {
+                    exceptions.Add(new ValidationException(901, $"Name Cannot be empty"));
+                }
+                if (exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }            
+        }
+
+        public void Delete(SystemCountryCodePoco[] pocos)
+        {
+            _repository.Remove(pocos);
+        }
+    }
+}
