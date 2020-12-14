90419ffdd082d81f3af28e3329437f8bdf13d6c5
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-13 21:54:45 -0500

    adding business logic for Applicant Resume Poco

---
+++ CareerCloud.BusinessLogicLayer/ApplicantResumeLogic.cs
@@ -0,0 +1,37 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantResumeLogic : BaseLogic<ApplicantResumePoco>
+    {
+        public ApplicantResumeLogic(IDataRepository<ApplicantResumePoco > repo) : base(repo)
+        {
+
+        }
+        public override void Add(ApplicantResumePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(ApplicantResumePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(ApplicantResumePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (ApplicantResumePoco poco in pocos)
+            {
+                if(string.IsNullOrEmpty(poco.Resume))
+                {
+                    exceptions.Add(new ValidationException(113, $"{poco.Id} - Resume cannot be empty"));
+                }
+            }
+        }
+    }
+}
