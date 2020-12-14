7401fe2a79c75a987a4282ab5ff9198ac96753cb
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 12:10:07 -0500

    modified the logic for phone number verification - applying only to numbers with - sign

--- CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
@@ -32,24 +32,51 @@ namespace CareerCloud.BusinessLogicLayer
                 {
                     exceptions.Add(new ValidationException(600, $"{poco.Id} - Invalid website"));
                 }
-                if(string.IsNullOrEmpty(poco.ContactPhone) || PhoneNumber.IsPhoneNbr(poco.ContactPhone))
+                //if(string.IsNullOrEmpty(poco.ContactPhone) || PhoneNumber.IsPhoneNbr(poco.ContactPhone))
+                //{
+                //    exceptions.Add(new ValidationException(601, $"{poco.Id} - Invalid phone number"));
+                //}
+                if (string.IsNullOrEmpty(poco.ContactPhone))
                 {
-                    exceptions.Add(new ValidationException(601, $"{poco.Id} - Invalid phone number"));
+                    exceptions.Add(new ValidationException(702, $"PhoneNumber for SecurityLogin {poco.Id} is required"));
                 }
-                if(exceptions.Count > 0)
+                else
+                {
+                    string[] phoneComponents = poco.ContactPhone.Split('-');
+                    if (phoneComponents.Length != 3)
+                    {
+                        exceptions.Add(new ValidationException(703, $"PhoneNumber for SecurityLogin {poco.Id} is not in the required format."));
+                    }
+                    else
+                    {
+                        if (phoneComponents[0].Length != 3)
+                        {
+                            exceptions.Add(new ValidationException(703, $"PhoneNumber for SecurityLogin {poco.Id} is not in the required format."));
+                        }
+                        else if (phoneComponents[1].Length != 3)
+                        {
+                            exceptions.Add(new ValidationException(703, $"PhoneNumber for SecurityLogin {poco.Id} is not in the required format."));
+                        }
+                        else if (phoneComponents[2].Length != 4)
+                        {
+                            exceptions.Add(new ValidationException(703, $"PhoneNumber for SecurityLogin {poco.Id} is not in the required format."));
+                        }
+                    }
+                }
+                if (exceptions.Count > 0)
                 {
                     throw new AggregateException(exceptions);
                 }
             }
         }
     }
-    public static class PhoneNumber
-    {
-        public const string phoneregex = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+    //public static class PhoneNumber
+    //{
+    //    public const string phoneregex = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
 
-        public static bool IsPhoneNbr(string number)
-        {
-            return Regex.IsMatch(number, phoneregex);
-        }
-    }
+    //    public static bool IsPhoneNbr(string number)
+    //    {
+    //        return Regex.IsMatch(number, phoneregex);
+    //    }
+    //}
 }
