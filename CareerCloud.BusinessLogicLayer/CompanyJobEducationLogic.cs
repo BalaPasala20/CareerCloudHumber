ce5787674e418781ee584b6044da3a87a740ffe7
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 10:15:41 -0500

    adding business logic for CompanyJobEducationPoco

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobEducationLogic.cs
@@ -0,0 +1,42 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobEducationLogic : BaseLogic<CompanyJobEducationPoco>
+    {
+        public CompanyJobEducationLogic(IDataRepository<CompanyJobEducationPoco> repo) : base(repo)
+        {
+
+        }
+        public override void Add(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(CompanyJobEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyJobEducationPoco poco in pocos)
+            {
+                if(string.IsNullOrEmpty(poco.Major) || poco.Major.Length<=2)
+                {
+                    exceptions.Add(new ValidationException(200, $"{poco.Id} - Major must be atleast 2 characters"));
+                }
+                if(poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(201, $"{poco.Id} - Importance cannot be less than 0"));
+                }
+
+            }
+        }
+    }
+}
