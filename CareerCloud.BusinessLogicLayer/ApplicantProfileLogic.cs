c43cfc7b1b367042b1145fa98b27e8b7aec2c09c
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-13 21:49:01 -0500

    adding business logic for applicant profile poco

---
+++ CareerCloud.BusinessLogicLayer/ApplicantProfileLogic.cs
@@ -0,0 +1,46 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantProfileLogic : BaseLogic<ApplicantProfilePoco>
+    {
+        public ApplicantProfileLogic(IDataRepository<ApplicantProfilePoco> repo) : base(repo)
+        {
+
+        }
+        public override void Add(ApplicantProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(ApplicantProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(ApplicantProfilePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (ApplicantProfilePoco poco in pocos)
+            {
+                if(poco.CurrentSalary < 0)
+                {
+                    exceptions.Add(new ValidationException(111, $"{poco.Id} - CurrentSalary cannot be negative"));
+                }
+                if(poco.CurrentRate < 0)
+                {
+                    exceptions.Add(new ValidationException(112, $"{poco.Id} - CurrentRate cannot be negative"));
+
+                }
+                if(exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
