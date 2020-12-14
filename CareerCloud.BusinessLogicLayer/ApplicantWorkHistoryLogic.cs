99e8e75057e44a480cc8a6b0df6b9c8c087b7716
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-13 22:14:30 -0500

    adding business logic for Applicant Work History poco

---
+++ CareerCloud.BusinessLogicLayer/ApplicantWorkHistoryLogic.cs
@@ -0,0 +1,41 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantWorkHistoryLogic : BaseLogic<ApplicantWorkHistoryPoco>
+    {
+        public ApplicantWorkHistoryLogic(IDataRepository<ApplicantWorkHistoryPoco> repo) : base(repo)
+        {
+
+        }
+        public override void Add(ApplicantWorkHistoryPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(ApplicantWorkHistoryPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(ApplicantWorkHistoryPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (ApplicantWorkHistoryPoco poco in pocos)
+            {
+                if(string.IsNullOrEmpty(poco.CompanyName) || poco.CompanyName.Length < 2 )
+                {
+                    exceptions.Add(new ValidationException(105, $"{poco.Id} - Company name should be more than 2 characters"));
+                }
+                if(exceptions.Count >0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
