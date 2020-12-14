2b5d5670a27644970cbb332acfead1a88f69f9f7
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 10:23:20 -0500

    adding business logic for CompanyJobSkillPoco

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobSkillLogic.cs
@@ -0,0 +1,42 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobSkillLogic : BaseLogic<CompanyJobSkillPoco>
+    {
+        public CompanyJobSkillLogic(IDataRepository<CompanyJobSkillPoco> repo) : base (repo)
+        {
+                    
+        }
+        public override void Add(CompanyJobSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(CompanyJobSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(CompanyJobSkillPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyJobSkillPoco poco in pocos)
+            {
+                if(poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(400, $"{poco.Id} - Importance cannot be less than 0"));
+                }
+                if(exceptions.Count >0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+
+            }
+        }
+    }
+}
