d74d228d0e6dc7e7006cb559eef1a33c8439231c
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-14 10:05:54 -0500

    adding business logic for CompanyJobDescriptionPoco

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobDescriptionLogic.cs
@@ -0,0 +1,45 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobDescriptionLogic : BaseLogic<CompanyJobDescriptionPoco>
+    {
+        public CompanyJobDescriptionLogic(IDataRepository<CompanyJobDescriptionPoco> repo) : base(repo)
+        {
+
+        }
+        public override void Add(CompanyJobDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(CompanyJobDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+        protected override void Verify(CompanyJobDescriptionPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyJobDescriptionPoco poco in pocos)
+            {
+                if(string.IsNullOrEmpty(poco.JobName))
+                {
+                    exceptions.Add(new ValidationException(300, $"{poco.Id} - Job name cannot be empty"));
+                }
+                if(string.IsNullOrEmpty(poco.JobDescriptions))
+                {
+                    exceptions.Add(new ValidationException(301, $"{poco.Id} - Job description cannot be empty"));
+                }
+                if(exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
