f645b2d9dc7b78cd720845131a559d64aa6663d4
Bala Pasala <balaanthony1219@gmail.com>
2021-01-15 23:25:22 -0500

    adding Dbset for all the poco classes and overriding onconfiguring class

--- CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,27 @@ namespace CareerCloud.EntityFrameworkDataAccess
 {
     public class CareerCloudContext : DbContext
     {
+        public DbSet<ApplicantEducationPoco> ApplicantEducations { get; set; }
+        public DbSet<ApplicantJobApplicationPoco> ApplicantJobApplications { get; set; }
+        public DbSet<ApplicantProfilePoco> ApplicantProfiles { get; set; }
+        public DbSet<ApplicantResumePoco> ApplicantResumes { get; set; }
+        public DbSet<ApplicantSkillPoco> ApplicantSkills { get; set; }
+        public DbSet<ApplicantWorkHistoryPoco> ApplicantWorkHistories { get; set; }
+        public DbSet<CompanyDescriptionPoco> CompanyDescriptions { get; set; }
+        public DbSet<CompanyJobDescriptionPoco> CompanyJobDescriptions { get; set; }
+        public DbSet<CompanyJobEducationPoco> CompanyJobEducations { get; set; }
+        public DbSet<CompanyJobPoco> CompanyJobs { get; set; }
+        public DbSet<CompanyJobSkillPoco> CompanyJobskills { get; set; }
+        public DbSet<CompanyLocationPoco> CompanyLocations { get; set; }
+        public DbSet<CompanyProfilePoco> CompanyProfiles { get; set; }
+        public DbSet<SecurityLoginPoco> SecurityLogins { get; set; }
+        public DbSet<SecurityLoginsLogPoco> SecurityLoginsLogs { get; set; }
+        public DbSet<SecurityLoginsRolePoco> SecurityLoginsRoles { get; set; }
+        public DbSet<SecurityRolePoco> SecurityRoles { get; set; }
+        public DbSet<SystemCountryCodePoco> SystemCountryCodes { get; set; }
+        public DbSet<SystemLanguageCodePoco> SystemLanguageCodes { get; set; }
+        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("connection string");
