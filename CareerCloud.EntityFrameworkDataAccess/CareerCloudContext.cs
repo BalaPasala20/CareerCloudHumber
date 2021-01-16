a2d9ada4c0ef84976479a109a967500b2ff463fe
Bala Pasala <balaanthony1219@gmail.com>
2021-01-15 22:50:52 -0500

    adding a class library for Data access through entity framework

---
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    public class CareerCloudContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connection string");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
