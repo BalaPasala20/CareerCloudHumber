e3bb3ad94b98b26aefb9e15d3f2b967240fd48dc
Bala Pasala <balaanthony1219@gmail.com>
2021-01-18 22:42:57 -0500

    finished CRUD related methods

---
+++ CareerCloud.EntityFrameworkDataAccess/EFGenericRepository.cs
@@ -0,0 +1,83 @@
+﻿using CareerCloud.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    public class EFGenericRepository<T> : IDataRepository<T> where T : class
+    {
+        private CareerCloudContext _context;
+
+        public EFGenericRepository()
+        {
+            _context = new CareerCloudContext();
+        }
+        public void Add(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                //_context.ApplicantEducations.Add()
+                _context.Entry(item).State = EntityState.Added;
+            }
+            _context.SaveChanges();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll(params System.Linq.Expressions.Expression<Func<T, object>>[] navigationProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            foreach (Expression<Func<T, object>> navProperty in navigationProperties)
+            {
+                query = query.Include<T, object>(navProperty);
+            }
+            return query.ToList();
+        }
+
+        public IList<T> GetList(System.Linq.Expressions.Expression<Func<T, bool>> where, params System.Linq.Expressions.Expression<Func<T, object>>[] navigationProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            foreach (Expression<Func<T, object>> navProperty in navigationProperties)
+            {
+                query = query.Include<T, object>(navProperty);
+            }
+            return query.Where(where).ToList();
+        }
+
+        public T GetSingle(System.Linq.Expressions.Expression<Func<T, bool>> where, params System.Linq.Expressions.Expression<Func<T, object>>[] navigationProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            foreach(Expression<Func<T,object>>navProperty in navigationProperties)
+            {
+                query = query.Include<T, object>(navProperty);
+            }
+            return query.Where(where).FirstOrDefault();
+            //same as return query.FirstOrDefault(where);
+        }
+
+        public void Remove(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                _context.Entry(item).State = EntityState.Deleted;
+            }
+            _context.SaveChanges();
+        }
+
+        public void Update(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                _context.Entry(item).State = EntityState.Modified;
+            }
+            _context.SaveChanges();
+        }
+    }
+}
