25a1853b273b7235ff1a4ef22d03024d8c087103
Bala Pasala <balaanthony1219@gmail.com>
2021-02-14 00:03:38 -0500

    done - System related controllers

---
+++ CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using CareerCloud.ADODataAccessLayer;
+using System.Web.Http.Description;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/system/v1")]
+    [ApiController]
+    public class SystemCountryCodeController : ControllerBase
+    {
+        private readonly SystemCountryCodeLogic _logic;
+
+        public SystemCountryCodeController()
+        {
+            EFGenericRepository<SystemCountryCodePoco> repo = new EFGenericRepository<SystemCountryCodePoco>();
+            _logic = new SystemCountryCodeLogic(repo);
+        }
+        [HttpGet]
+        [Route("country/{roleId}")]
+        [ResponseType(typeof(SystemCountryCodePoco))]
+        public ActionResult GetSystemCountryCode(string code)
+        {
+            SystemCountryCodePoco poco = _logic.Get(code);
+            if (poco == null)
+            {
+                return NotFound();
+            }
+            return Ok(poco);
+        }
+        [HttpGet]
+        [Route("country")]
+        [ResponseType(typeof(List<SystemCountryCodePoco>))]
+        public ActionResult GetAllSystemCountryCode()
+        {
+            List<SystemCountryCodePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                return NotFound();
+            }
+            return Ok(pocos);
+        }
+        [HttpPost]
+        [Route("country")]
+        public ActionResult PostSystemCountryCode([FromBody] SystemCountryCodePoco[] pocos)
+        {
+            try
+            {
+                _logic.Add(pocos);
+                return Ok();
+            }
+            catch (AggregateException ae)
+            {
+                return BadRequest(ae);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPut]
+        [Route("country")]
+        public ActionResult PutSystemCountryCode([FromBody] SystemCountryCodePoco[] pocos)
+        {
+            try
+            {
+                _logic.Update(pocos);
+                return Ok();
+            }
+            catch (AggregateException ae)
+            {
+                return BadRequest(ae);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpDelete]
+        [Route("country")]
+        public ActionResult DeleteSystemCountryCode([FromBody] SystemCountryCodePoco[] pocos)
+        {
+            _logic.Delete(pocos);
+            return Ok();
+        }
+    }
+}
---
+++ CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using CareerCloud.ADODataAccessLayer;
+using System.Web.Http.Description;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/system/v1")]
+    [ApiController]
+    public class SystemLanguageCodeController : ControllerBase
+    {
+        private readonly SystemLanguageCodeLogic _logic;
+
+        public SystemLanguageCodeController()
+        {
+            EFGenericRepository<SystemLanguageCodePoco> repo = new EFGenericRepository<SystemLanguageCodePoco>();
+            _logic = new SystemLanguageCodeLogic(repo);
+        }
+        [HttpGet]
+        [Route("language/{languageId}")]
+        [ResponseType(typeof(SystemLanguageCodePoco))]
+        public ActionResult GetSystemLanguageCode(string languageId)
+        {
+            SystemLanguageCodePoco poco = _logic.Get(languageId);
+            if (poco == null)
+            {
+                return NotFound();
+            }
+            return Ok(poco);
+        }
+        [HttpGet]
+        [Route("language")]
+        [ResponseType(typeof(List<SystemLanguageCodePoco>))]
+        public ActionResult GetAllSystemLanguageCode()
+        {
+            List<SystemLanguageCodePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                return NotFound();
+            }
+            return Ok(pocos);
+        }
+        [HttpPost]
+        [Route("language")]
+        public ActionResult PostSystemLanguageCode([FromBody] SystemLanguageCodePoco[] pocos)
+        {
+            try
+            {
+                _logic.Add(pocos);
+                return Ok();
+            }
+            catch (AggregateException ae)
+            {
+                return BadRequest(ae);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPut]
+        [Route("language")]
+        public ActionResult PutSystemLanguageCode([FromBody] SystemLanguageCodePoco[] pocos)
+        {
+            try
+            {
+                _logic.Update(pocos);
+                return Ok();
+            }
+            catch (AggregateException ae)
+            {
+                return BadRequest(ae);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpDelete]
+        [Route("language")]
+        public ActionResult DeleteSystemLanguageCode([FromBody] SystemLanguageCodePoco[] pocos)
+        {
+            _logic.Delete(pocos);
+            return Ok();
+        }
+    }
+}
